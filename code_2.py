84d2184b5706fc234ec9d25a4bca429f05567cda
Ada <139880994+Adamay01@users.noreply.github.com>
2024-01-19 21:51:01 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+    return data
+
+
+    data = generate_random_data()
+import random
+def generate_random_data():
+def main():
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
